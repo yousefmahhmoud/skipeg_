4f9faacfff19598b19691a3a8afc1d44d356ae7e
yousefmahhmoud <319200239@sha.eg.com>
2025-07-27 02:37:04 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Slideshow from './components/Slideshow';
 import ProductsSection from './components/ProductsSection';
 import AboutSection from './components/AboutSection';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
+      <Analytics />
       <main>
         <Slideshow />
         <ProductsSection />
